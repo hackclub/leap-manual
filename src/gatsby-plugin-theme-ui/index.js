778056f90fcc64433e56059190bff19bbe263c1d
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-08 13:15:11 +0800

    let there be light!!

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -32,7 +32,7 @@ const theme = {
   breakpoints: [32, 48, 64, 96, 128].map((w) => `${w}em`),
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   fontSizes: [12, 16, 20, 24, 32, 48, 64, 96, 128, 160, 192],
-  initialColorModeName: "dark",
+  initialColorModeName: "light",
   useColorSchemeMediaQuery: false,
   colors: {
     ...colors,
